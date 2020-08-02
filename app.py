ba51a2a969c34eda488f65314b59df79fabf39d3
Roland Jansky <roland.jansky@gmail.com>
2020-08-02 17:32:15 +0200

    adding requirements and procfile.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # app.py
 from flask import Flask, request, jsonify
+
 app = Flask(__name__)
+
 
 @app.route('/getmsg/', methods=['GET'])
 def respond():
@@ -25,26 +27,30 @@ def respond():
     # Return the response in json format
     return jsonify(response)
 
+
 @app.route('/post/', methods=['POST'])
 def post_something():
     param = request.form.get('name')
     print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the
+    # POST functionality
     if param:
         return jsonify({
             "Message": f"Welcome {name} to our awesome platform!!",
             # Add this option to distinct the POST request
-            "METHOD" : "POST"
+            "METHOD": "POST"
         })
     else:
         return jsonify({
             "ERROR": "no name found, please send a name."
         })
 
+
 # A welcome message to test our server
 @app.route('/')
 def index():
     return "<h1>Welcome to our server !!</h1>"
+
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
